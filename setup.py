d41c5cde1be4fcc322f639aeab8c37106be4fe2f
Naudith_Florez <Naudith.florez@est.iudigital.edu.co>
2025-02-27 13:34:49 -0500

    actividad_1 - configuracion

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-
-
 from setuptools import setup, find_packages
 
 setup(
-    name="pad_2025",
+    name="pad",
     version="0.0.1",
-    author="Naudith_Florez",
+    author="Naudith Florez",
     author_email="Naudith.florez@est.iudigital.edu.co",
     description="",
     py_modules=["actividad_1"],
     install_requires=[
         "pandas",
-        "matplotlib"
-    ])
+        "openpyxl",
+        "requests"
+    ]
+    
+    
+)
